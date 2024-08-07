953cfd92c622a2d1a29fd68f5fb9b0782d6550ae
Brwa Yaseen <brwayasin29@gmail.com>
2024-08-07 15:48:39 +0300

    no mas

--- src/app/(dashboard)/postspage/page.tsx
+++ src/app/(dashboard)/postspage/page.tsx
@@ -1,7 +1,9 @@
 const Page = () => {
   return (
-    <div>
-      <h1 className="flex justify-center items-center mt-8">users Page</h1>
+    <div className="h-full w-full">
+      <h1 className="flex justify-center h-full w-full items-center mt-8">
+        posts Page
+      </h1>
     </div>
   );
 };
